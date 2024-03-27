418284334eba6eb0f3824640ebd2aae27575c2f9
wongprom <>
2024-03-27 11:06:40 +0100

    Create class Paddle...
    Create Paddel class from Super class Turtle. Make it possible to create multible Paddles with diferent positions. Create function to move paddle up and down.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from turtle import Screen, Turtle
-
-# STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+from turtle import Screen
+from paddle import Paddle
 
 # Screen
 screen = Screen()
@@ -10,29 +9,17 @@ screen.title("Pong Game")
 screen.tracer(0)
 
 # Paddle
-paddle = Turtle()
-paddle.color("white")
-paddle.shape("square")
-paddle.penup()
-paddle.shapesize(stretch_wid=5, stretch_len=1, outline=1)
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
+# Move Paddles
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
-game_is_on= True
+game_is_on = True
 while game_is_on:
     screen.update()
 
