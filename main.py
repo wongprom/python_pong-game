abd8a2b39afafbab0735d2e3817bd97e0480332c
wongprom <>
2024-03-27 10:16:27 +0100

    Create and move paddle.

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
+# STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+
+# Screen
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
+
+# Paddle
+paddle = Turtle()
+paddle.color("white")
+paddle.shape("square")
+paddle.penup()
+paddle.shapesize(stretch_wid=5, stretch_len=1, outline=1)
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on= True
+while game_is_on:
+    screen.update()
+
 
 screen.exitonclick()
